6f654752a114745b5efa9712dab782da8d7c1711
ayokunlejames <129544207+ayokunlejames@users.noreply.github.com>
2025-02-08 12:46:25 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,22 +2,26 @@ import streamlit as st
 import time
 
 def main():
-    st.title("💕 Timi, Will You Be My Valentine? 💕")
+    # Title with bigger text
+    st.markdown("<h1 style='font-size: 40px;'>💕 Timi, Will You Be My Valentine? 💕</h1>", unsafe_allow_html=True)
     
+    # Image with larger size
     st.image("IMG_3103.jpeg", use_container_width=True)
     
-    st.write("Hey baby, I just wanted to ask you a quick question...")
-    st.write("Will you be my Valentine? 🌚💖")
+    # Write text with custom size
+    st.markdown("<h2 style='font-size: 30px;'>Hey baby, I just wanted to ask you a quick question...</h2>", unsafe_allow_html=True)
+    st.markdown("<h3 style='font-size: 25px;'>Will you be my Valentine? 🌚💖</h3>", unsafe_allow_html=True)
     
     col1, col2 = st.columns(2)
     
+    # Customize buttons with bigger size using custom HTML
     with col1:
-        if st.button("Yes! 💘"):
+        if st.button("Yes! 💘", key="yes_button"):
             st.balloons()
             st.success("Yay! I knew you'd say yes! 😍")
     
     with col2:
-        if st.button("No...😡"):
+        if st.button("No...😡", key="no_button"):
             st.write("Wrong answer..let's try this again uhn 🫠")
             time.sleep(2)
             st.experimental_rerun()
