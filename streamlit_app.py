5467e7279f4cb1b9cdd01c82841ca4c5a9d55dd0
ayokunlejames <129544207+ayokunlejames@users.noreply.github.com>
2025-02-08 13:05:01 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,8 +5,12 @@ def main():
     # Title with bigger text
     st.markdown("<h1 style='font-size: 40px;'>💕 Timi baby, Will You Be My Valentine? 💕</h1>", unsafe_allow_html=True)
     
-    # Image with larger size
-    # st.image("IMG_3103.jpeg", use_container_width=True)
+   
+     # Display a slider to control the image size
+    image_width = st.slider("Select image width", min_value=100, max_value=1000, value=500)
+
+    # Display the image with the selected width
+    st.image("IMG_3103.jpeg", width=image_width)
     
     # Write text with custom size
     # st.markdown("<h2 style='font-size: 30px;'>Hey baby, I just wanted to ask you a quick question...</h2>", unsafe_allow_html=True)
