b4e06518c5ec7f3d3df7e368eb25966bdf465885
ayokunlejames <129544207+ayokunlejames@users.noreply.github.com>
2025-02-08 12:41:50 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,36 +2,12 @@ import streamlit as st
 import time
 
 def main():
-    st.markdown(
-        f"""
-        <style>
-            .stApp {{
-                background: url('IMG_3103.jpeg') no-repeat center center fixed;
-                background-size: cover;
-            }}
-            .stApp::before {{
-                content: """";
-                position: absolute;
-                top: 0;
-                left: 0;
-                width: 100%;
-                height: 100%;
-                background: rgba(0, 0, 0, 0.3); /* Adjust transparency here */
-                z-index: -1;
-            }}
-            h1 {{
-                text-align: center;
-                font-size: 50px;
-                color: white;
-            }}
-        </style>
-        """,
-        unsafe_allow_html=True
-    )
+    st.title("💕 Timi, Will You Be My Valentine? 💕")
     
-    st.markdown("""<h1>💕 Timi, Will You Be My Valentine? 💕</h1>""", unsafe_allow_html=True)
+    st.image("IMG_3103.jpeg", use_container_width=True)
     
     st.write("Hey baby, I just wanted to ask you a quick question...")
+    st.write("Will you be my Valentine? 🌚💖")
     
     col1, col2 = st.columns(2)
     
@@ -41,7 +17,7 @@ def main():
             st.success("Yay! I knew you'd say yes! 😍")
     
     with col2:
-        if st.button("No... 😡"):
+        if st.button("No...😡"):
             st.write("Wrong answer..let's try this again uhn 🫠")
             time.sleep(2)
             st.experimental_rerun()
